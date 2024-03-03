7812d3e94dd8bdacf4e7cdc9d6120b6da5547e9b
Ava242 <1905022@ugrad.cse.buet.ac.bd>
2024-03-03 11:02:08 +0600

    edit tag page modified

--- src/routes/(user)/profile/edit-profile/+page.svelte
+++ src/routes/(user)/profile/edit-profile/+page.svelte
@@ -7,6 +7,9 @@
   import FaIcon from "$lib/components/FaIcon.svelte";
   import Spinner from "$lib/components/Spinner.svelte";
 
+  import ButtonComp from "$lib/components/Button.svelte";
+
+
   export let data;
 
   let success = false;
@@ -69,6 +72,23 @@
           updateFace(); // Update the DOM/file input
       }
   };
+
+  // async function performAction() {
+  //       loading.set(true);
+  //       await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate an async operation
+  //       loading.set(false);
+  //       message.set("Operation completed."); // Example of setting a message post-operation
+  //   }
+
+    async function handleClick() {
+        console.log('Button clicked');
+        // Add your click handling logic here
+
+        loading.set(true);
+        await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate an async operation
+        loading.set(false);
+        message.set("Operation completed."); // 
+    }
 </script>
 
   
@@ -90,7 +110,7 @@
       </p>
     </div>
   {/if}
-
+<!-- 
   {#if loading}
   <div
     class="text-center text-lg flex flex-row justify-center items-center"
@@ -98,7 +118,7 @@
     Loading... <Spinner size={4} className="ml-2" />
   </div>
 {/if}
-   
+    -->
   <form
   class="request-item flex flex-col bg-black-100 border-2 border-orange-200 p-4 rounded-lg shadow-lg hover:bg-orange-150 transition-colors duration-200 ease-in-out"
   method="POST"
@@ -221,9 +241,28 @@
       <h3 class="font-bold mb-2.5">Phone Number</h3>
       <textarea class="w-full h-16 rounded border p-2 mb-4 bg-orange-100 hover:bg-orange-200" placeholder="Type your Phone number here" name="phonenumber" bind:value={$phonenumber}></textarea>
     </div>
-  
-    <button class="w-20 h-10 px-4 py-2 m-4 bg-green-500 text-white rounded hover:bg-green-600" on:click={(event) => editAbout( event)}>Save</button>
+    
+    <ButtonComp text="Submit" {loading} onClick={handleClick} />
+
+    <!-- <button class="w-20 h-10 px-4 py-2 m-4 bg-green-500 text-white rounded hover:bg-green-600" on:click={(event) => editAbout( event)}>Save</button> -->
     <!-- </form>     -->
+
+    <!-- <button
+        class="w-20 h-10 px-4 py-2 m-4 bg-green-500 text-white rounded hover:bg-green-600 text-white font-bold py-2 px-4 rounded flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed"
+        on:click={performAction}
+        disabled={loading}
+    >
+        {#if loading}
+            Tailwind CSS Spinner -->
+            <!-- <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            Loading...
+        {:else}
+            Save -->
+        <!-- {/if}
+    </button> --> 
     </form>
    </div>
   </div>
